b130b28e2e60ea78d7e92d96cace6efbc1b85676
Jeremy Vinding <jeremy.vinding@closely.com>
2014-04-10 16:17:37 -0600

    [https://trello.com/c/nYveLnug] first attempt to support multiple commits per push

--- lib/listen.rb
+++ lib/listen.rb
@@ -25,31 +25,17 @@ class GithubTrello < Sinatra::Base
     short_code = ''
     begin
       if params[:payload]
-        Trello.logger.debug("has payload:\n#{params[:payload]}")
-
         push = JSON.parse(params[:payload])
 
-        Trello.logger.debug("parsed github json\n#{push}")
+        cards = 0
 
-        short_code = trello_short_code(push['head_commit']['message'])
-        card = trello_card(short_code) if short_code
+        repo = push['repository']['name']
 
-        Trello.logger.debug("card: #{card}")
-
-        if short_code && card
-          head_commit = push['head_commit']
-          author = head_commit['author']['name']
-          message = head_commit['message']
-          hash = head_commit['id']
-          compare_url = push['compare']
-          repo = push['repository']['name']
-          comment = formatted_comment(repo, author, hash, message, compare_url)
-          Trello.logger.debug("About to add comment\n#{comment}")
-          card.add_comment(comment)
-          response = "Commit: #{comment}"
-        else
-          response = "No card found"
+        push.commits.each do |commit|
+          cards += handle_commit(repo, commit)
         end
+
+        response = "#{cards} cards found"
       else
         status 500
         response = "missing payload"
@@ -62,6 +48,32 @@ class GithubTrello < Sinatra::Base
     return response
   end
 
+  def handle_commit(repo, commit_json)
+    Trello.logger.debug("Handling commit\n#{commit_json}")
+
+    short_code = trello_short_code(commit_json['message'])
+    card = trello_card(short_code) if short_code
+
+    Trello.logger.debug("card: #{card}")
+
+    count = 0
+    if short_code && card
+      count = 1
+      author = commit_json['author']['name']
+      message = commit_json['message']
+      hash = commit_json['id']
+      compare_url = commit_json['url']
+      comment = formatted_comment(repo, author, hash, message, compare_url)
+      Trello.logger.debug("About to add comment\n#{comment}")
+      card.add_comment(comment)
+      response = "Commit: #{comment}"
+    else
+      response = "No card found"
+    end
+    
+    return count
+  end
+  
   def trello_card(code)
     Trello.logger.debug("About to find card #{code}")
     @card ||= Trello::Card.find(code)
@@ -74,7 +86,7 @@ class GithubTrello < Sinatra::Base
   end
 
   def formatted_comment(repo, author_name, hash, message, diff_url)
-    <<-EOF.gsub /^ {4}/, ''
+    <<-EOF.gsub(/^ {4}/, '')
       **Commit to #{repo} by #{author_name} [#{hash[0..8]}](#{diff_url})**:
 
       ```
